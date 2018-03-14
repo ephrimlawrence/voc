36378ba4697360539da6a5c2a731c70fa1713921
Lawrence Ephrim <ephrimlawrence@gmail.com>
2018-03-14 05:42:53 +0000

    Added test for dict.keys() method
    The test was taken from https://github.com/pybee/ouroboros/blob/master/ouroboros/test/test_dict.py

--- tests/builtins/test_dict.py
+++ tests/builtins/test_dict.py
@@ -1,8 +1,21 @@
-from .. utils import TranspileTestCase, BuiltinFunctionTestCase
+from ..utils import BuiltinFunctionTestCase
+from ..utils import TranspileTestCase
 
 
 class DictTests(TranspileTestCase):
-    pass
+
+    def test_keys(self):
+        d = {}
+        self.assertEqual(set(d.keys()), set())
+        d = {'a': 1, 'b': 2}
+        k = d.keys()
+        self.assertEqual(set(k), {'a', 'b'})
+        self.assertIn('a', k)
+        self.assertIn('b', k)
+        self.assertIn('a', d)
+        self.assertIn('b', d)
+        self.assertRaises(TypeError, d.keys, None)
+        self.assertEqual(repr(dict(a=1).keys()), "dict_keys(['a'])")
 
 
 class BuiltinDictFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
